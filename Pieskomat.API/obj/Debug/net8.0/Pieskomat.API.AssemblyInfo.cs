52282f79d32dda2b03efbf7a7cf735443696ae3f
pecet <pecet3107@gmail.com>
2024-03-12 13:16:23 +0100

    .

--- Pieskomat.API/obj/Debug/net8.0/Pieskomat.API.AssemblyInfo.cs
+++ Pieskomat.API/obj/Debug/net8.0/Pieskomat.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pieskomat.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+639071cbf64a98c2ba10b61851a321c87f823f03")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pieskomat.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pieskomat.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
